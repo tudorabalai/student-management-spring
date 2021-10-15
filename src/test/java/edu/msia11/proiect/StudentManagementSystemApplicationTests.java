0ceec466fdd8415868d97774e1744df7e1139c99
tudorabalai <tudorabalai@gmail.com>
2021-10-15 23:56:22 +0300

    First commit

---
+++ src/test/java/edu/msia11/proiect/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.msia11.proiect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
