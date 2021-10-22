c8563a7c3745bce702a93c01f4091cb98303da8f
tudorabalai <tudorabalai@gmail.com>
2021-10-22 20:11:10 +0300

    - Add fields

--- src/main/java/edu/msia11/proiect/components/student/StudentEntity.java
+++ src/main/java/edu/msia11/proiect/components/student/StudentEntity.java
@@ -3,5 +3,24 @@ package edu.msia11.proiect.components.student;
 /* Entitatea este reprezentarea unei tabele din baza de date
 * sub forma unei clase Java */
 public class StudentEntity {
+    private String numarMatricol;
+    private String Nume;
+    private String Prenume;
+    private long cnp;
+    private String adresaDomiciliu;
+    private String serieBuletin;
+    private int sumarBuletin;
+
+    public String getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public void setNumarMatricol(String numarMatricol) {
+        this.numarMatricol = numarMatricol;
+    }
+
+    public String getNume() {
+        return Nume;
+    }
 
 }
