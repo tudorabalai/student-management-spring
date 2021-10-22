5fba4603850d3b33a64608ab03592feeace61606
tudorabalai <tudorabalai@gmail.com>
2021-10-22 20:11:52 +0300

    - Add getter and setters

--- src/main/java/edu/msia11/proiect/components/student/StudentEntity.java
+++ src/main/java/edu/msia11/proiect/components/student/StudentEntity.java
@@ -23,4 +23,47 @@ public class StudentEntity {
         return Nume;
     }
 
+    public void setNume(String nume) {
+        Nume = nume;
+    }
+
+    public String getPrenume() {
+        return Prenume;
+    }
+
+    public void setPrenume(String prenume) {
+        Prenume = prenume;
+    }
+
+    public long getCnp() {
+        return cnp;
+    }
+
+    public void setCnp(long cnp) {
+        this.cnp = cnp;
+    }
+
+    public String getAdresaDomiciliu() {
+        return adresaDomiciliu;
+    }
+
+    public void setAdresaDomiciliu(String adresaDomiciliu) {
+        this.adresaDomiciliu = adresaDomiciliu;
+    }
+
+    public String getSerieBuletin() {
+        return serieBuletin;
+    }
+
+    public void setSerieBuletin(String serieBuletin) {
+        this.serieBuletin = serieBuletin;
+    }
+
+    public int getSumarBuletin() {
+        return sumarBuletin;
+    }
+
+    public void setSumarBuletin(int sumarBuletin) {
+        this.sumarBuletin = sumarBuletin;
+    }
 }
