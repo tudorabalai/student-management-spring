ed8ca3996a2b7b42f43fcfe9017e96c3e591be7a
tudorabalai <tudorabalai@gmail.com>
2021-10-29 23:19:00 +0300

    Create DTOs

--- src/main/java/edu/msia11/proiect/components/student/input/StudentInputDTO.java
+++ src/main/java/edu/msia11/proiect/components/student/input/StudentInputDTO.java
@@ -1,6 +1,25 @@
 package edu.msia11.proiect.components.student.input;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /* Aceasta clasa DTO (Data Transfer Object) o vei folosi
 * ca model pentru input (cand vrei sa adaugi un nou student) */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentInputDTO {
+
+    private Long cnp;
+
+    private Integer numarCI;
+
+    private String serieCI;
+    private String nrMatricol;
+    private String nume;
+    private String prenume;
+    private String adresaCompleta;
 }
--- src/main/java/edu/msia11/proiect/components/student/output/StudentOutputDTO.java
+++ src/main/java/edu/msia11/proiect/components/student/output/StudentOutputDTO.java
@@ -1,6 +1,27 @@
 package edu.msia11.proiect.components.student.output;
 
+import edu.msia11.proiect.components.student.StudentEntity;
+import lombok.*;
+
+import javax.persistence.Id;
+import java.util.Objects;
+
 /* Aceasta clasa DTO (Data Transfer Object) o vei folosi
  * ca model pentru output (cand vrei sa afisezi un anumit student) */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentOutputDTO {
+
+    private Long id;
+    private Long cnp;
+
+    private Integer numarCI;
+
+    private String serieCI;
+    private String nrMatricol;
+    private String nume;
+    private String prenume;
+    private String adresaCompleta;
 }
