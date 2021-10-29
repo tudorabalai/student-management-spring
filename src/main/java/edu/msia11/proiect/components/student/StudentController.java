ca0e3074c300b4c863e80b61f6bedecc2a3af37c
tudorabalai <tudorabalai@gmail.com>
2021-10-29 23:19:47 +0300

    Mark controller with annotation

--- src/main/java/edu/msia11/proiect/components/student/StudentController.java
+++ src/main/java/edu/msia11/proiect/components/student/StudentController.java
@@ -1,6 +1,9 @@
 package edu.msia11.proiect.components.student;
 
+import org.springframework.stereotype.Controller;
+
 /* Controller-ul este partea accesibila public
 * care serveste  obiectele tale catre utilizator/front end */
+@Controller
 public class StudentController {
 }
