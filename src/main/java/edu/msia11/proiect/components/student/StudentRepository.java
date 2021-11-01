5a97253806d46e0bf88407cde25ab8d4279d8f1f
tudorabalai <tudorabalai@gmail.com>
2021-11-01 14:46:24 +0200

    Include more methods for repository

--- src/main/java/edu/msia11/proiect/components/student/StudentRepository.java
+++ src/main/java/edu/msia11/proiect/components/student/StudentRepository.java
@@ -4,10 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /* Repository-ul este partea care te ajuta sa NU scrii SQL.
 * Aici se petrece magia.*/
 @Repository
 @Transactional
 public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
+
+    List<StudentEntity> findAllByNumeIgnoreCase(String name);
+
+    List<StudentEntity> findAllByPrenumeIgnoreCase(String name);
+
+    List<StudentEntity> findAllByCodNumericPersonal(Long cnp);
 }
